73c72013be1ab927c30177867eeacca72dff4312
qudalsrnt3x <qudalsrnt3x@gmail.com>
2021-06-25 22:00:22 +0900

    비즈니스 계층 CRUD

---
+++ src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,45 @@
+package org.zerock.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
+
+import java.util.List;
+
+@Service
+@Log4j
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService{
+
+    private final BoardMapper boardMapper;
+
+    @Override
+    public void register(BoardVO boardVO) {
+        boardMapper.insertSelectKey(boardVO);
+    }
+
+    @Override
+    public BoardVO get(Long bno) {
+        return boardMapper.read(bno);
+    }
+
+    @Override
+    public List<BoardVO> getList() {
+        return boardMapper.getList();
+    }
+
+    @Override
+    public boolean modify(BoardVO boardVO) {
+        int update = boardMapper.update(boardVO);
+
+        return update == 1;
+    }
+
+    @Override
+    public boolean remove(Long bno) {
+
+        return boardMapper.delete(bno) == 1;
+    }
+}
