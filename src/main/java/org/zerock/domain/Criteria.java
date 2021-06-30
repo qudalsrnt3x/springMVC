ec6ae3d7c2a659447ebc7b7de2196605ab6bba45
qudalsrnt3x <qudalsrnt3x@gmail.com>
2021-06-30 23:35:11 +0900

    검색 처리 -> BoardMapper.xml, BoardMapperTest.java 에서 테스트

--- src/main/java/org/zerock/domain/Criteria.java
+++ src/main/java/org/zerock/domain/Criteria.java
@@ -12,6 +12,9 @@ public class Criteria {
     private int pageNum;
     private int amount;
 
+    private String type;
+    private String keyword;
+
     // 기본값 (1 페이지, 10개씩)
     public Criteria() {
         this(1, 10);
@@ -20,6 +23,11 @@ public class Criteria {
     public Criteria(int pageNum, int amount) {
         this.pageNum = pageNum;
         this.amount = amount;
+    }
+
+    public String[] getTypeArr() {
+
+        return type == null ? new String[]{} : type.split("");
     }
 
 }
